0638541860d491e621ec319131bd9148f20cfdc7
r0227n <developryo@gmail.com>
2023-01-10 05:56:06 +0900

    edit: extends Text Widget

--- flutter/todo_box/lib/pages/components/emoji_text.dart
+++ flutter/todo_box/lib/pages/components/emoji_text.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
 
-class EmojiText extends StatelessWidget {
-  const EmojiText(this.data, {super.key});
-
-  final String data;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox.square(
-      dimension: 24,
-      child: FittedBox(
-        child: Text(
-          data,
-          style: const TextStyle(
-            color: Colors.white,
-            fontFamily: 'EmojiOne',
-          ),
-          textAlign: TextAlign.center,
-        ),
-      ),
-    );
-  }
+class EmojiText extends Text {
+  const EmojiText(
+    super.data, {
+    super.key,
+    super.strutStyle,
+    super.style = const TextStyle(
+      color: Colors.white,
+      fontSize: 24.0,
+      fontFamily: 'EmojiOne',
+    ),
+    super.textAlign = TextAlign.center,
+    super.textDirection,
+    super.locale,
+    super.softWrap,
+    super.overflow,
+    super.textScaleFactor,
+    super.maxLines,
+    super.semanticsLabel,
+    super.textWidthBasis,
+    super.textHeightBehavior,
+    super.selectionColor,
+  });
 }
