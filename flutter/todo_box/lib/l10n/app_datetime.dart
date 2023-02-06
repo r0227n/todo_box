04a793674c0acb4295be4458a0a700ab32ba2905
r0227n <developryo@gmail.com>
2023-02-07 07:09:22 +0900

    add: DateTimeの変換処理を追加

--- flutter/todo_box/lib/l10n/app_datetime.dart
+++ flutter/todo_box/lib/l10n/app_datetime.dart
@@ -5,7 +5,7 @@ extension LocalString on DateTime {
   /// [AppLocalizations] ごとの時刻表記に変換
   String formatLocal(AppLocalizations localizations) {
     /// 日本語 / 日本
-    if (localizations.localeName == 'ja') {
+    if (isJp(localizations)) {
       // 時刻未選択
       if (hour == 0 && minute == 0 && second == 0 && millisecond == 0) {
         return DateFormat('MMMEd', 'ja').format(this);
@@ -17,4 +17,18 @@ extension LocalString on DateTime {
     ///　英語 / アメリカ
     return DateFormat('MMM, d, E, HH:mm').format(this);
   }
+
+  String toMMMEd(AppLocalizations localizations) {
+    if (isJp(localizations)) {
+      return DateFormat.MMMEd('ja').format(this);
+    }
+
+    return DateFormat.MMMEd().format(this);
+  }
+
+  String toHHmm(AppLocalizations localizations) {
+    return DateFormat('HH:mm').format(this);
+  }
+
+  bool isJp(AppLocalizations localizations) => localizations.localeName == 'ja';
 }
